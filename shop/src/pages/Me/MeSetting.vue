df610a1fb9cafe10302028e9e727e65eea22f3bb
Danny <2233879049@qq.com>
2020-10-03 11:38:50 +0800

    vueshop

---
+++ shop/src/pages/Me/MeSetting.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="me-setting">
+    <div>
+      <me-common-cell left-icon="itlike-2" left-title="免密码支付" />
+      <me-common-cell
+        left-icon="itlike-3"
+        left-title="免拼单设置"
+        :clickCell="dealCellClick"
+      />
+      <me-common-cell
+        left-icon="itlike-1"
+        left-title="消息接收设置"
+        right-title="接收"
+        right-title-color="red"
+      />
+    </div>
+
+    <div style="margin-top: 10px">
+      <me-common-cell
+        left-icon="itlike-5"
+        left-title="免密码支付"
+        :clickCell="dealCellClick"
+      />
+      <me-common-cell left-icon="itlike-4" left-title="免密码支付" />
+    </div>
+
+    <div style="margin-top: 10px">
+      <me-common-cell left-icon="itlike-1" left-title="免密码支付" />
+    </div>
+
+    <div class="logout" @click="dealLogout">退出登录</div>
+    <button class="login-back" @click="$router.back()">返回</button>
+  </div>
+</template>
+
+<script>
+import MeCommonCell from "./MeCommonCell";
+import { Toast, MessageBox } from "mint-ui";
+import { mapActions } from "vuex";
+
+export default {
+  name: "MeSetting",
+  components: {
+    MeCommonCell,
+  },
+  methods: {
+    ...mapActions(["logOut"]),
+    dealCellClick(props) {
+      Toast({
+        message: "点击了" + props,
+        duration: 5000,
+        showCancelButton: true,
+      });
+      // MessageBox.confirm("", {
+      //   title: "adfaf",
+      //   message: "asdfadfaf",
+      //   confirmButtonText: "sd",
+      //   cancelButtonText: false,
+      //   showCancelButton: false,
+      // });
+    },
+    dealLogout() {
+      // console.error(11);
+      MessageBox.confirm("您确定退出登录吗?").then((action) => {
+        // console.log(action);
+        if ("confirm" === action) {
+          // 退出登录
+          let result = this.logOut({});
+          // console.log(result);
+          // 回到主界面
+          this.$router.replace("/home");
+        }
+      });
+    },
+  },
+};
+</script>
+
+<style scoped lang="stylus" ref="stylesheet/stylus">
+
+
+.login-back,.logout {
+  display: block;
+  width: 100%;
+  height: 44px;
+  margin-top: 10px;
+  border-radius: 4px;
+  background-color: #fff;
+  display: flex;
+   justify-content: center;
+  align-items: center;
+  text-align: center;
+  background: transparent;
+  border: 1px solid mediumpurple;
+  color: mediumpurple;
+  text-align: center;
+  font-size: 16px;
+  line-height: 42px;
+}
+</style>
---
+++ shop/src/store/actions.js
@@ -0,0 +1,126 @@
+import {
+  getHomeCasual,
+  getHomeNav,
+  getHomeShopList,
+  getRecommendShopList,
+  getSearchGoods,
+  getUserInfo,
+  getLogout,
+  getCartsGoods,
+  delCartGoods
+} from '../api'
+
+import {
+  HOME_CASUAL,
+  HOME_NAV,
+  HOME_SHOP_LIST,
+  RECOMMEND_SHOP_LIST,
+  SEARCH_GOODS,
+  USER_INFO,
+  RESET_USER_INFO,
+
+  CART_GOODS_LIST,
+  ADD_GOODS_COUNT,
+  REDUCE_GOODS_COUNT,
+  SELECTED_SINGER_GOODS,
+  SELECTED_ALL_GOODS,
+  DEL_SINGER_GOODS,
+  DEL_CART_GOODS
+} from './mutation-types'
+
+export default {
+  // 1. 获取首页的轮播图
+  async reqHomeCasual({commit}) {
+    const result = await getHomeCasual();
+    commit(HOME_CASUAL, {homecasual: result.message})
+  },
+
+  // 2. 获取首页的导航
+  async reqHomeNav({commit}) {
+    const result = await getHomeNav();
+    commit(HOME_NAV, {homenav: result.message.data})
+  },
+
+  // 3. 获取首页的商品列表
+  async reqHomeShopList({commit}) {
+    const result = await getHomeShopList();
+    commit(HOME_SHOP_LIST, {homeshoplist: result.message.goods_list})
+  },
+
+  // 4. 获取推荐的商品数据
+  async reqRecommendShopList({commit}, params) {
+
+    const result = await getRecommendShopList(params);
+    commit(RECOMMEND_SHOP_LIST, {recommendshoplist: result.message});
+    params.callback && params.callback();
+  },
+
+  // 5. 获取推荐的商品数据
+  async reqSearchGoods({commit}, callback) {
+    const result = await getSearchGoods();
+    commit(SEARCH_GOODS, {searchgoods: result.message.data});
+    callback && callback();
+  },
+
+  // 6. 同步用户的信息
+  syncUserInfo({commit}, userInfo){
+    commit(USER_INFO, {userInfo});
+  },
+
+  // 7. 异步获取用户信息
+  async getUserInfo({commit}){
+    const result = await getUserInfo();
+    console.log(result);
+    if(result.success_code === 200){
+       commit(USER_INFO, {userInfo: result.message});
+    }
+  },
+
+  // 8. 退出登录
+  async logOut({commit}){
+    const result = await getLogout();
+    console.log(result);
+    if(result.success_code === 200){
+      commit(RESET_USER_INFO);
+    }
+  },
+
+    // 9. 请求购物车数据
+    async reqCartsGoods({commit}) {
+      const result = await getCartsGoods();
+      if(result.success_code === 200){
+        commit(CART_GOODS_LIST, {cartgoods: result.message})
+      }
+    },
+    // 9. 请求购物车数据
+    async delCartsGoods({commit}) {
+      const result = await delCartGoods();
+      if(result.success_code === 200){
+        commit(DEL_CART_GOODS, {cartgoods: result.message})
+      }
+    },
+
+  // 9. 购物车单个商品的增加和减少
+  updateGoodsCount({commit}, {goods, isAdd}) {
+    if(isAdd){ // 增加
+        commit(ADD_GOODS_COUNT, {goods});
+    }else { // 减少
+       commit(REDUCE_GOODS_COUNT, {goods});
+    }
+},
+
+// 10. 单个商品的选中和取消
+singerGoodsSelected({commit}, {goods}){
+  commit(SELECTED_SINGER_GOODS, {goods});
+},
+
+// 11. 全选和取消全选
+selectedAll({commit}, {isSelected}){
+  commit(SELECTED_ALL_GOODS, {isSelected});
+},
+
+// 12. 删除单个商品
+delGoodsSinger({commit}, {goods}){
+   commit(DEL_SINGER_GOODS, {goods});
+}
+}
